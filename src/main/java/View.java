4b7837fdffbf8481764aa2d585985f5f06e10488
sarangsavani <70777360+sarangsavani@users.noreply.github.com>
2020-12-15 17:50:54 -0500

    cleaning things up

--- src/main/java/View.java
+++ src/main/java/View.java
@@ -1,14 +1,18 @@
 
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
 
 public class View extends JFrame implements ActionListener{
     
     View(){
+        
         
 
       JMenuBar menubar = new JMenuBar();
@@ -24,20 +28,33 @@ public class View extends JFrame implements ActionListener{
         //Create the help tab
         JMenu help = new JMenu("Help");     //Help button
         menubar.add(help);
+        JMenuItem about = new JMenuItem("Please choose a food.");
+        help.add(about);
 
         // Exits the app
-        JMenu exit = new JMenu("Exit");        //Exit button
+        JMenu exit = new JMenu("Exit");        //Exit button       
         menubar.add(exit);
-        //View mainView = new View();
+      
+       
             
         JButton test = new JButton("test3");
         test.addActionListener(this);
           
-          //create the menu bar
-        
+          //creates the menu bar
         this.setJMenuBar(menubar);
-        menubar.setVisible(true);
         
+       
+        class actionexit {
+            public void actionPreformed (ActionEvent e){
+                System.exit(1);
+                
+                
+                
+              
+                
+               
+        }
+       
         
         //frame of the program
         this.setTitle("FoodApp");
@@ -48,45 +65,16 @@ public class View extends JFrame implements ActionListener{
     public void actionPreformed(ActionEvent e){
             System.out.print("Test2");
         }
+    
 
     @Override
     public void actionPerformed(ActionEvent e) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-}
-
-        /*
-       Panels() {
-           
-        JFrame window = new JFrame("1");
-        JPanel pane1_01 = new JPanel();
-        JPanel pane1_02 = new JPanel();
-        JButton click = new JButton();
-        JLabel text = new JLabel("2");
-         
-        
-       
-
-            JFrame frame1 = new JFrame("test1");
-            frame1.setSize(500, 500);
-            frame1.setVisible(true);
-
-            JFrame frame2 = new JFrame("test2");
-            frame2.setSize(500, 500);
-            frame2.setVisible(true);
-             
-              pane1_02.setBackground(color.AQUA);
-                        
-                window.add(pane1_01, BorderLayout.CENTER);
-                window.add(pane1_02, BorderLayout.PAGE_END);
-                
-                window.setSize(400, 400);
-                window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                window.setVisible(true);
-            }
-             
-
-
     
-
-*/
+    
+    
+    
+    
+            }
+            
