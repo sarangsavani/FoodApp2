dc5165ba639803aee9907b253982bb43cd0602af
sarangsavani <70777360+sarangsavani@users.noreply.github.com>
2020-12-15 18:26:57 -0500

    added comments

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,6 @@
+
+
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -16,6 +19,9 @@ public class App {
     public static void main(String args[]){ 
         
         System.out.println("test1");
-        View mainView = new View();
+        
+       View mainView = new View();
+       
+       
     }
 }
